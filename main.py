5cda57ab9b90f200e83b42ea7bd754b0eb1fa963
Dominic <41173009+AstroDomDev@users.noreply.github.com>
2024-03-19 03:18:04 +0000

    prepare for backend development, set up basic routes.

---
+++ main.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+import os
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(os.path.abspath(os.path.dirname(__file__)), 'data', 'app.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/gallery')
+def gallery():
+    return render_template('gallery.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
